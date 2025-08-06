de5a178d965205edb40822c0b59ee3e1f3dcaee6
AndreeaRistea <ristea.andreea.k5r@student.ucv.ro>
2025-08-07 00:01:56 +0300

    Reapply "3&4 Folders config & Backup folder daily"
    This reverts commit 24423aa5b6aa657a9cea816fd5cf501a53aed71e.

---
+++ Backuparr/BgService/BackupBgService.cs
@@ -0,0 +1,28 @@
+﻿
+using System.IO.Compression;
+
+namespace Backuparr.BgService
+{
+    public class BackupBgService : BackgroundService
+    {
+        private readonly string sourceFolder = Environment.GetEnvironmentVariable("SOURCE_FOLDER");
+        private readonly string backupFolder = Environment.GetEnvironmentVariable("DESTINATION_FOLDER");
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                if (DateTime.UtcNow.Hour != 0)
+                {
+                    await Task.Delay(60000, stoppingToken);
+                    continue;
+                }
+
+                string today = DateTime.Now.ToString("yyyy-MM-dd");
+                string zipPath = Path.Combine(backupFolder, $"backup-{today}.zip");
+
+                ZipFile.CreateFromDirectory(sourceFolder, zipPath, CompressionLevel.Optimal, includeBaseDirectory: false);
+            }
+        }
+    }
+}
